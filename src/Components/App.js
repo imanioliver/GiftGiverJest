e744f801539a3123cdf894c658bf7c577e339e3a
Imani Oliver <imani.oliver@gmail.com>
2018-01-25 10:07:38 -0500

    4 passing  tests, and a button that maps over gifts in the gift array. Uses ES6 to find max id

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = { gifts: [] };
+  }
+
+  addGift = () => {
+    const { gifts } = this.state;
+
+    const ids = this.state.gifts.map(gift => gift.id);
+
+    const max_id = ids.length > 0 ? Math.max(...ids) : 0;
+
+    gifts.push({ id: max_id+1 }); //we want the id of the object to be unique
+
+    this.setState({ gifts })
+  }
+
   render() {
     return (
       <div>
         <h2>Gift Giver</h2>
+        <div className='gift-list'>
+        {
+          this.state.gifts.map(gift => {
+            return (
+            <div key={gift.id}></div>)
+          })
+        }
+
+        </div>
+        <Button className="btn-add" onClick={this.addGift}>Add Gift</Button>
       </div>
     )
   }
